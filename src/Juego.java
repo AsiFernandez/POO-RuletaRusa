0215f4a4d57393775f9da62b26a3ef14191f7b2a
ik013043z1 <ik013043z1@hz341916-206-02.H013043.NET>
2018-12-17 09:20:27 +0100

    Clase juego creada desde 0 con todos los metodos necesarios

--- src/Juego.java
+++ src/Juego.java
@@ -1,4 +1,79 @@
 
 public class Juego {
 
+	private Jugador[] jugadores;
+	private Revolver revolver;
+	
+	/*
+	 * GETTERS Y SETTERS
+	 */
+	
+	public Jugador[] getJugadores() {
+		return jugadores;
+	}
+
+	public void setJugadores(Jugador[] jugadores) {
+		this.jugadores = jugadores;
+	}
+
+	public Revolver getRevolver() {
+		return revolver;
+	}
+
+	public void setRevolver(Revolver revolver) {
+		this.revolver = revolver;
+	}
+	
+	
+	/*
+	 * METODOS
+	 */
+	
+	//metodo para crear la partida con la cantidad de jugadores que tiene
+	public Juego (int numJugadores) {
+		jugadores = new Jugador[comprobarJugadores(numJugadores)];
+	}
+	
+	
+	  //metodo para comprobar que los jugadores son
+	  //entre 1 y 6, en caso de no estar en ese rango
+	  //se predefinira como 6 
+	 
+	private int comprobarJugadores(int numJugadores) {
+		
+		if(!(numJugadores >= 1 && numJugadores <= 6)) {
+			numJugadores = 6;
+		}
+		
+		return numJugadores;
+	}
+	
+	
+	  //crea y rellena el array 
+	  //con la cantidad de jugadores que se haya introducido
+	 
+	private void crearJugadores() {
+		for(int i= 0; i < jugadores.length; i++) {
+			jugadores[i] = new Jugador(i + 1);
+		}
+	}
+	
+	
+	/*
+	 * FUNCIONES
+	 */
+	
+	//Funcion de cada ronda, 
+	//una vez el jugador esta muerto 
+	public void ronda() {
+		for(int i= 0; i<jugadores.length; i++) {
+			jugadores[i].disparar(revolver);
+			
+			if(!jugadores[i].estaVivo()) {
+				return;
+			}
+		}
+	}
+	
+	
 }
